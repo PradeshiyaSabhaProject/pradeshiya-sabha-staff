d873fa9d6c2c5c906fbdb210dee621dc919d9583
Pesandu Rashodya <pesandurashodya@gmail.com>
2026-07-01 19:37:59 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Project Initialized in Current Directory!
+      </h1>
+    </div>
+  )
+}
